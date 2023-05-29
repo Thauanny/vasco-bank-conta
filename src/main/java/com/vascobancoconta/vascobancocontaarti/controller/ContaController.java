dd57a7ea3c8072189d6b467ce35ced295474ef47
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2023-05-29 16:21:07 -0300

    ajuste swagger

--- src/main/java/com/vascobancoconta/vascobancocontaarti/controller/ContaController.java
+++ src/main/java/com/vascobancoconta/vascobancocontaarti/controller/ContaController.java
@@ -3,6 +3,7 @@ package com.vascobancoconta.vascobancocontaarti.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +22,21 @@ public class ContaController {
     // @Autowired
     // private ContaService contaService;
 
-    @PostMapping("/criar/{idUser}")
+    @PostMapping("/{idUser}")
     public ResponseEntity<?> cadastrarConta(@PathVariable Integer idUser, @RequestBody Conta conta) {
+        try {
+            System.out.println(idUser);
+            // chamada service
+            return ResponseEntity.ok(conta);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("{\"message\": \"Nao foi possivel concluir\"}");
+        }
+
+    }
+
+    @PutMapping("/{idUser}")
+    public ResponseEntity<?> atualizarConta(@PathVariable Integer idUser, @RequestBody Conta conta) {
         try {
             System.out.println(idUser);
             // chamada service
@@ -36,6 +50,19 @@ public class ContaController {
 
     @GetMapping("/{idConta}")
     public ResponseEntity<?> conta(@PathVariable Integer idConta) {
+        try {
+            // chamada service
+            System.out.println(idConta);
+            return ResponseEntity.ok("{\"message\": \"Em contrucao\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("{\"message\": \"Nao foi possivel concluir\"}");
+        }
+
+    }
+
+    @DeleteMapping("/{idConta}")
+    public ResponseEntity<?> deleteConta(@PathVariable Integer idConta) {
         try {
             // chamada service
             System.out.println(idConta);
